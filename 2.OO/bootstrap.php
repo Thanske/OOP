7c5b53f1f612d86ae63a2104b97ae0b99cd8a815
Thanske <Steven.thans@telenet.be>
2022-02-21 20:11:55 +0100

    Update bootstrap.php

--- 2.OO/bootstrap.php
+++ 2.OO/bootstrap.php
@@ -6,8 +6,8 @@ $configuration = array(
     'db_pass' => null,
 );
 
-require_once __DIR__.'/lib/Service/Container.php';
 require_once __DIR__.'/lib/Model/Ship.php';
 require_once __DIR__.'/lib/Service/BattleManager.php';
 require_once __DIR__.'/lib/Service/ShipLoader.php';
 require_once __DIR__.'/lib/Model/BattleResult.php';
+require_once __DIR__.'/lib/Service/Container.php';
